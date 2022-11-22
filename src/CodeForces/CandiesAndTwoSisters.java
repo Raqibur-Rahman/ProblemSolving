f71beba7eca299291241f683d91cf1666db56809
Raqibur-Rahman <raqibur.a@gmail.com>
2022-11-22 09:11:47 +0600

    CF problem A, WA because of higher time complexity

--- src/CodeForces/CandiesAndTwoSisters.java
+++ src/CodeForces/CandiesAndTwoSisters.java
@@ -5,16 +5,14 @@ import java.util.Scanner;
 public class CandiesAndTwoSisters {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int tc = s.nextInt();
 
+        int tc = s.nextInt();
         while (tc > 0) {
             int n = s.nextInt();
             int count = 0;
-            for (int i = n; i >= n / 2; i--) {
-                for (int j = 1; j <= n / 2; j++) {
-                    if (i + j == n)
-                        count++;
-                }
+            for (int i = n-1, j = 1; j<=n/2 ; i--, j++) {
+                if (i + j == n && i>j)
+                    count++;
             }
             System.out.println(count);
 
