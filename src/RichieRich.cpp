0d1652a49cc019609c758848f7411c3494c244c7
Md. Raqibur Rahman Roni <46454601+Raqibur-Rahman@users.noreply.github.com>
2022-12-05 21:46:59 +0600

    CodeChef Easy problem .cpp

---
+++ src/RichieRich.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,x;
+        cin>>a>>b>>x;
+        cout<<(b-a)/x;
+
+    }
+    return 0;
+}
+
